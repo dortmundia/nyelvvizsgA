44a06c9fb96feab206869a5199cb2d044676de3c
dortmundia <csulienjatek@gmail.com>
2021-01-06 13:21:14 +0100

    órai munka

---
+++ src/com/company/Lista.java
@@ -0,0 +1,134 @@
+package com.company;
+
+public class Lista {
+    private String Nyelv;
+    private int SikeresEv1;
+    private int SikeresEv2;
+    private int SikeresEv3;
+    private int SikeresEv4;
+    private int SikeresEv5;
+    private int SikeresEv6;
+    private int SikeresEv7;
+    private int SikeresEv8;
+    private int SikeresEv9;
+    private int SikeresEv10;
+
+    private int SikertelenEv1;
+    private int SikertelenEv2;
+    private int SikertelenEv3;
+    private int SikertelenEv4;
+    private int SikertelenEv5;
+    private int SikertelenEv6;
+    private int SikertelenEv7;
+    private int SikertelenEv8;
+    private int SikertelenEv9;
+    private int SikertelenEv10;
+
+    public Lista(String nyelv, int sikeresEv1, int sikeresEv2, int sikeresEv3, int sikeresEv4, int sikeresEv5, int sikeresEv6, int sikeresEv7, int sikeresEv8, int sikeresEv9, int sikeresEv10, int sikertelenEv1, int sikertelenEv2, int sikertelenEv3, int sikertelenEv4, int sikertelenEv5, int sikertelenEv6, int sikertelenEv7, int sikertelenEv8, int sikertelenEv9, int sikertelenEv10) {
+        Nyelv = nyelv;
+        SikeresEv1 = sikeresEv1;
+        SikeresEv2 = sikeresEv2;
+        SikeresEv3 = sikeresEv3;
+        SikeresEv4 = sikeresEv4;
+        SikeresEv5 = sikeresEv5;
+        SikeresEv6 = sikeresEv6;
+        SikeresEv7 = sikeresEv7;
+        SikeresEv8 = sikeresEv8;
+        SikeresEv9 = sikeresEv9;
+        SikeresEv10 = sikeresEv10;
+        SikertelenEv1 = sikertelenEv1;
+        SikertelenEv2 = sikertelenEv2;
+        SikertelenEv3 = sikertelenEv3;
+        SikertelenEv4 = sikertelenEv4;
+        SikertelenEv5 = sikertelenEv5;
+        SikertelenEv6 = sikertelenEv6;
+        SikertelenEv7 = sikertelenEv7;
+        SikertelenEv8 = sikertelenEv8;
+        SikertelenEv9 = sikertelenEv9;
+        SikertelenEv10 = sikertelenEv10;
+    }
+
+    public String getNyelv() {
+        return Nyelv;
+    }
+
+    public int getSikeresEv1() {
+        return SikeresEv1;
+    }
+
+    public int getSikeresEv2() {
+        return SikeresEv2;
+    }
+
+    public int getSikeresEv3() {
+        return SikeresEv3;
+    }
+
+    public int getSikeresEv4() {
+        return SikeresEv4;
+    }
+
+    public int getSikeresEv5() {
+        return SikeresEv5;
+    }
+
+    public int getSikeresEv6() {
+        return SikeresEv6;
+    }
+
+    public int getSikeresEv7() {
+        return SikeresEv7;
+    }
+
+    public int getSikeresEv8() {
+        return SikeresEv8;
+    }
+
+    public int getSikeresEv9() {
+        return SikeresEv9;
+    }
+
+    public int getSikeresEv10() {
+        return SikeresEv10;
+    }
+
+    public int getSikertelenEv1() {
+        return SikertelenEv1;
+    }
+
+    public int getSikertelenEv2() {
+        return SikertelenEv2;
+    }
+
+    public int getSikertelenEv3() {
+        return SikertelenEv3;
+    }
+
+    public int getSikertelenEv4() {
+        return SikertelenEv4;
+    }
+
+    public int getSikertelenEv5() {
+        return SikertelenEv5;
+    }
+
+    public int getSikertelenEv6() {
+        return SikertelenEv6;
+    }
+
+    public int getSikertelenEv7() {
+        return SikertelenEv7;
+    }
+
+    public int getSikertelenEv8() {
+        return SikertelenEv8;
+    }
+
+    public int getSikertelenEv9() {
+        return SikertelenEv9;
+    }
+
+    public int getSikertelenEv10() {
+        return SikertelenEv10;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,117 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    private static ArrayList<Lista>nyelvek;
+
+    public static void main(String[] args) {
+
+        try {
+            beolvasas();
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("sikertelen beolvasás!");
+            return;
+        }
+            //2.feladat
+        int i = 0;
+        int top1=0;
+        int top1szam=0;
+        int top2szam=0;
+        int top3szam=0;
+        int top2=0;
+        int top3=0;
+        while (i<nyelvek.size())
+        {
+            int asd = nyelvek.get(i).getSikeresEv1()+ nyelvek.get(i).getSikeresEv2()+nyelvek.get(i).getSikeresEv3()+nyelvek.get(i).getSikeresEv4()+nyelvek.get(i).getSikeresEv5()+nyelvek.get(i).getSikeresEv6()+nyelvek.get(i).getSikeresEv7()+nyelvek.get(i).getSikeresEv8()+nyelvek.get(i).getSikeresEv9()+nyelvek.get(i).getSikeresEv10()+nyelvek.get(i).getSikertelenEv1()+nyelvek.get(i).getSikertelenEv2()+nyelvek.get(i).getSikertelenEv3()+nyelvek.get(i).getSikertelenEv4()+nyelvek.get(i).getSikertelenEv5()+nyelvek.get(i).getSikertelenEv6()+nyelvek.get(i).getSikertelenEv7()+nyelvek.get(i).getSikertelenEv8()+nyelvek.get(i).getSikertelenEv9()+nyelvek.get(i).getSikertelenEv10();
+            if(asd>top1) // (Feltételezheti, hogy nem alakult ki holtverseny.)
+            {
+                top3=top2;
+                top3szam=top2szam;
+                top2szam=top1szam;
+                top2=top1;
+                top1=asd;
+                top1szam=i;
+
+            }
+            else if (asd>top2)
+            {
+                top3=top2;
+                top3szam=top2szam;
+                top2szam=i;
+                top2=asd;
+            }
+            else if (asd>top3)
+            {
+                top3=asd;
+                top3szam=i;
+            }
+
+            i++;
+        }
+
+        System.out.println("A legnépszerűbb nyelvek:");
+        System.out.println(nyelvek.get(top1szam).getNyelv());
+        System.out.println(nyelvek.get(top2szam).getNyelv());
+        System.out.println(nyelvek.get(top3szam).getNyelv());
+
+
+
+    }
+
+    private static void beolvasas() throws FileNotFoundException {
+
+        nyelvek = new ArrayList<>();
+
+        Scanner sc = new Scanner(new File("sikeres.csv"));
+        Scanner sc2 = new Scanner(new File("sikertelen.csv"));
+
+        sc.nextLine();
+        sc2.nextLine();
+
+        while (sc.hasNext())
+        {
+            String[] sor = sc.nextLine().split(";");
+            String[] sor2 = sc2.nextLine().split(";");
+            String nyelv = sor[0];
+            int ev1 = Integer.parseInt(sor[1]);
+            int ev2 = Integer.parseInt(sor[2]);
+            int ev3 = Integer.parseInt(sor[3]);
+            int ev4 = Integer.parseInt(sor[4]);
+            int ev5 = Integer.parseInt(sor[5]);
+            int ev6 = Integer.parseInt( sor[6]);
+            int ev7 = Integer.parseInt(sor[7]);
+            int ev8 = Integer.parseInt( sor[8]);
+            int ev9 = Integer.parseInt(sor[9]);
+            int ev10 =Integer.parseInt( sor[10]);
+
+            int SikertelenEv1 =Integer.parseInt(sor2[1]);
+            int SikertelenEv2=Integer.parseInt(sor2[2]);
+            int SikertelenEv3=Integer.parseInt(sor2[3]);
+            int SikertelenEv4=Integer.parseInt(sor2[4]);
+            int SikertelenEv5=Integer.parseInt(sor2[5]);
+            int SikertelenEv6=Integer.parseInt(sor2[6]);
+            int SikertelenEv7=Integer.parseInt(sor2[7]);
+            int SikertelenEv8=Integer.parseInt(sor2[8]);
+            int SikertelenEv9=Integer.parseInt(sor2[9]);
+            int SikertelenEv10=Integer.parseInt(sor2[10]);
+
+            Lista adottnyelv = new Lista(nyelv,ev1,ev2,ev3,ev4,ev5,ev6,ev7,ev8,ev9,ev10,SikertelenEv1,SikertelenEv2,SikertelenEv3,SikertelenEv4,SikertelenEv5,SikertelenEv6,SikertelenEv7,SikertelenEv8,SikertelenEv9,SikertelenEv10);
+
+            nyelvek.add(adottnyelv);
+        }
+
+        sc.close();
+        sc2.close();
+
+    }
+
+
+
+}
